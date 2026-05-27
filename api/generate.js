a23fad3f3fef6f6126f38593daf81455421c2d0f
Ayush Mittal <ayushmittal629@gmail.com>
2026-05-27 13:58:47 +0530

    model changed

--- api/generate.js
+++ api/generate.js
@@ -49,7 +49,7 @@ export default async function handler(req) {
           "Content-Type": "application/json",
         },
         body: JSON.stringify({
-          model: "llama3-70b-8192",
+          model: "llama3-3-70b-versatile",
           messages: [
             {
               role: "system",
