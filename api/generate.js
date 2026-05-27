6ecad83bf56be075e63db7c6a45d5122e04fe221
Ayush Mittal <ayushmittal629@gmail.com>
2026-05-27 14:03:15 +0530

    llma to groq model conversion

--- api/generate.js
+++ api/generate.js
@@ -49,7 +49,7 @@ export default async function handler(req) {
           "Content-Type": "application/json",
         },
         body: JSON.stringify({
-          model: "llama3-3-70b-versatile",
+          model: "openai/gpt-oss-120b",
           messages: [
             {
               role: "system",
